9de166afe53bce656f5fd1d2849ff18a37a83a81
Ingo <roessner@infai.org>
2023-07-10 10:34:37 +0200

    remove faul-with-body from health.sh

--- conf/health.sh
+++ conf/health.sh
@@ -1,5 +1,5 @@
 if [[ -z "${CAMUNDA_APP_PASSWORD}" ]]; then
-  curl --fail-with-body http://localhost:8080/engine-rest/incident/count
+  curl --fail http://localhost:8080/engine-rest/incident/count
 else
-  curl --fail-with-body http://camunda:$CAMUNDA_APP_PASSWORD@localhost:8080/engine-rest/incident/count
+  curl --fail http://camunda:$CAMUNDA_APP_PASSWORD@localhost:8080/engine-rest/incident/count
 fi
