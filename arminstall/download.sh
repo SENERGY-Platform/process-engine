7fc0f7733bdd2df8af72fa8010ae229f484b500e
Ingo <roessner@infai.org>
2025-01-16 08:24:26 +0100

    fix armbuild of update to camunda 7.22

--- arminstall/download.sh
+++ arminstall/download.sh
@@ -48,12 +48,8 @@ mvn dependency:get -B --global-settings /tmp/settings.xml \
     -DgroupId="org.camunda.bpm" -DartifactId="camunda-database-settings" \
     -Dversion="${ARTIFACT_VERSION}" -Dpackaging="pom" -Dtransitive=false
 cambpmdbsettings_pom_file=$(find /m2-repository -name "camunda-database-settings-${ARTIFACT_VERSION}.pom" -print | head -n 1)
-MYSQL_VERSION=$(xmlstarlet sel -t -v //_:version.mysql $cambpmdbsettings_pom_file)
 POSTGRESQL_VERSION=$(xmlstarlet sel -t -v //_:version.postgresql $cambpmdbsettings_pom_file)
 
-mvn dependency:copy -B \
-    -Dartifact="mysql:mysql-connector-java:${MYSQL_VERSION}:jar" \
-    -DoutputDirectory=/tmp/
 mvn dependency:copy -B \
     -Dartifact="org.postgresql:postgresql:${POSTGRESQL_VERSION}:jar" \
     -DoutputDirectory=/tmp/
@@ -64,9 +60,6 @@ case ${DISTRO} in
 batch
 embed-server --std-out=echo
 
-module add --name=mysql.mysql-connector-java --slot=main --resources=/tmp/mysql-connector-java-${MYSQL_VERSION}.jar --dependencies=javax.api,javax.transaction.api
-/subsystem=datasources/jdbc-driver=mysql:add(driver-name="mysql",driver-module-name="mysql.mysql-connector-java",driver-xa-datasource-class-name=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource)
-
 module add --name=org.postgresql.postgresql --slot=main --resources=/tmp/postgresql-${POSTGRESQL_VERSION}.jar --dependencies=javax.api,javax.transaction.api
 /subsystem=datasources/jdbc-driver=postgresql:add(driver-name="postgresql",driver-module-name="org.postgresql.postgresql",driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)
 
@@ -76,7 +69,6 @@ EOF
         rm -rf /camunda/standalone/configuration/standalone_xml_history/current/*
         ;;
     *)
-        cp /tmp/mysql-connector-java-${MYSQL_VERSION}.jar /camunda/lib
         cp /tmp/postgresql-${POSTGRESQL_VERSION}.jar /camunda/lib
         # remove default CATALINA_OPTS from environment settings
         echo "" > /camunda/bin/setenv.sh
